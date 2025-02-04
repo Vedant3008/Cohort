0e9a985fad3d987aa6d19fe643d6d89acf6266a3
Vedant3008 <shahh7107@gmail.com>
2025-02-04 12:25:17 +0530

    js bascis

---
+++ Week-1-Landing Page/JSbasics.js
@@ -0,0 +1,15 @@
+//var is the older way to declare variable (avoid)
+
+//let helps to declare variables (reassignable but not in same scope)
+let user = "vedant";
+console.log(user);
+
+// const helps to declare variable that can not be reassignable
+const num = 7;
+console.log(num);
+
+//function
+
+
+
+
