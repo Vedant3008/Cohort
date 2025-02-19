6ad4b963d2bf6b02e8678293563b6a79eeef6482
Vedant3008 <shahh7107@gmail.com>
2025-02-19 23:25:58 +0530

    Class'  q '' clear Q R cd'

---
+++ Week-2-Async JS/IOAsynchronus.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("b.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+function run() {
+	console.log("I will run after 1s");
+}
+
+setTimeout(run, 1000);
+console.log("I will run immedietely");
---
+++ Week-2-Async JS/class-object.js
@@ -0,0 +1,31 @@
+class Rectangle {
+    constructor(width, height, color) {
+         this.width = width;
+         this.height = height;
+         this.color = color; 
+    }
+    
+    area() {
+        const area = this.width * this.height;
+          return area;
+    }
+    
+    paint() {
+             console.log('Painting with color'+" " +this.color);
+    }
+    
+ }
+ 
+ const rect = new Rectangle(2, 4,"Red")
+ const area = rect.area();
+ const color = rect.paint();
+ console.log(area);
+
+//
+
+const map = new Map();
+map.set('name','Vedant');
+map.set('age','21');
+
+const firstName = map.get('name');
+console.log(firstName)
