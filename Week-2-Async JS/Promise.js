d11904df01331f838df5f6069ac64f9edbcd1773
Vedant3008 <shahh7107@gmail.com>
2025-02-28 12:39:51 +0530

    Changes

---
+++ Week-2-Async JS/Promise.js
@@ -0,0 +1,25 @@
+// function setTimeoutPromisified(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+
+// const { Console } = require("console");
+
+  
+//   function callback() {
+//       console.log("3 seconds have passed");
+//   }
+  
+// setTimeoutPromisified(3000).then(callback)
+
+//
+// function vedant(resolve){
+//   setTimeout(resolve,3000);
+// }
+
+// let v = new Promise(vedant);
+
+// function callback(){
+//   console.log("Print this when complete");
+// }
+// v.then(callback);
+
