898cbed4e80cbfbc49206874afbbcc14e6e65d06
Vedant3008 <shahh7107@gmail.com>
2025-04-05 16:51:23 +0530

    HTTP

--- Week-4-Node/HTTP.js
+++ Week-4-Node/HTTP.js
@@ -3,4 +3,8 @@ const express = require("express");
 
 const app = express(); //creating hospital
 
+app.get("/", function(req,res){
+    res.send("Hello, Vedant");
+});
+
 app.listen(3000); //doctor listening to you in this room
