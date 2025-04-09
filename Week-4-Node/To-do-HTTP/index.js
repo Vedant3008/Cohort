167c0141f1a1529d4f662e5dcac7baa3fc5ab0f8
Vedant3008 <shahh7107@gmail.com>
2025-04-09 20:26:18 +0530

    To-do-HTTP

--- Week-4-Node/To-do-HTTP/index.js
+++ Week-4-Node/To-do-HTTP/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express()
-
+ 
+const todo = [];
 //route handlers
 app.get('/',function(req,res){
-    res.send('Hello World')
+    res.send('Hello, Add your to-do');
+})
+
+app.post('/add',function(req,res){
+    res.push({
+        id:
+        title
+    })
+})
+
+app.delete('/delete',function(req,res){
+    res.json({
+        id:1
+    })
 })
 
 app.listen(3000)//which port
