cf155b1903abe9ae6abe7f788d6fa601339da566
Vedant3008 <shahh7107@gmail.com>
2025-04-24 07:45:08 +0530

    Assignment

--- Week-5-Fetch/Assignment2.js
+++ Week-5-Fetch/Assignment2.js
@@ -1,2 +1,17 @@
 //Assignment #1 - Try converting the calculator assignment to use POST endpoints. 
 //Check if it works with/without the express.json middleware
+const express = require("express");
+const app = express()
+
+
+app.get("/sum", function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a + b
+    })
+})
+app.listen(3000);
+
+
