a2fe8880c7eff86ebb80c29ddab9f70b6158f56a
Vedant3008 <shahh7107@gmail.com>
2025-05-03 23:56:47 +0530

    Login

--- Week-6-Authentication/index.js
+++ Week-6-Authentication/index.js
@@ -34,6 +34,7 @@ app.post("/signup", function (req, res) {
     res.json({
       message: "You are signed up"
     });
+    console.log(users);
 });
   
 app.post("/signin", function (req, res) {
@@ -45,47 +46,29 @@ app.post("/signin", function (req, res) {
     if (user) {
       const token = generateToken();
       user.token = token;
-      res.send({ token });
+      res.send({ 
+        token 
+    });
     } else {
       res.status(403).send({ message: "Invalid username or password" });
     }
+    console.log(users);
+});
+
+app.get("/me", function(req, res){
+    const token = req.headers.token
+    const user = users.find(user => user.token === token);
+    if (user) {
+        res.send({
+            username: user.username
+    })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    };
 });
   
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000");
-  });
-/*app.post("/signup", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    users.push({
-        username: username,
-        password: password
-    })
-
-    res.json({
-        message: "You are signed in"
-    })
-
 });
-
-app.post("/signin", function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    const user = users.find(user => user.username === username && user.password === password);
-
-    if (user) {
-        const token = generateToken();
-        user.token = token;
-        res.send({
-            token
-        })
-        console.log(users);
-    } else {
-        res.status(403).send({
-            message: "Invalid username or password"
-        })
-    }
-});
-
-app.listen(3000);*/
